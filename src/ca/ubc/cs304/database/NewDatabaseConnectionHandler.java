5faa8bb285584c2cc746b9b1f4d1720839ac0732
Yeram Ko <yeramko@gmail.com>
2020-04-03 03:13:19 -0700

    queries uploaded in the handler file

--- src/ca/ubc/cs304/database/NewDatabaseConnectionHandler.java
+++ src/ca/ubc/cs304/database/NewDatabaseConnectionHandler.java
@@ -1,11 +1,10 @@
 package ca.ubc.cs304.database;
 
-import ca.ubc.cs304.model.BusinessModel;
-import ca.ubc.cs304.model.TravellerModel;
-import ca.ubc.cs304.model.UserModel;
+import ca.ubc.cs304.model.*;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class NewDatabaseConnectionHandler {
     // Use this version of the ORACLE_URL if you are running the code off of the server
@@ -45,6 +44,33 @@ public class NewDatabaseConnectionHandler {
         }
     }
 
+    //queries start
+
+
+    public void insertTraveller(TravellerModel model) {
+        try {
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO travellers VALUES (?,?,?,?,?,?,?)");
+            ps.setString(1, model.getUsername());
+            ps.setString(2, model.getName());
+            ps.setString(3, model.getCountry());
+            ps.setString(4, model.getProvince());
+            ps.setString(5, model.getCity());
+            ps.setString(6, model.getGender());
+            ps.setString(7, model.getDOB());
+
+            ps.executeUpdate();
+            connection.commit();
+
+            ps.close();
+
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+            rollbackConnection();
+        }
+
+    }
+
+
     public void deleteUser(UserModel user) {
         try {
             if (user instanceof TravellerModel) {
@@ -82,6 +108,29 @@ public class NewDatabaseConnectionHandler {
         }
     }
 
+
+    public void updateActivity(int id, String description) {
+        try {
+            PreparedStatement ps = connection.prepareStatement("UPDATE trav_grp_trp_activity SET Description = ? WHERE Activity_ID = ?");
+            ps.setInt(1, id);
+            ps.setString(2, description);
+
+            int rowCount = ps.executeUpdate();
+            if (rowCount == 0) {
+                System.out.println(WARNING_TAG + " Activity " + id + " does not exist!");
+            }
+
+            connection.commit();
+
+            ps.close();
+
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+            rollbackConnection();
+        }
+    }
+
+
     public BusinessModel[] getBusinessInfoBasedOnTitle(String title) {
         ArrayList<BusinessModel> result = new ArrayList<>();
         try {
@@ -106,6 +155,34 @@ public class NewDatabaseConnectionHandler {
         }
 
         return result.toArray(new BusinessModel[result.size()]);
+    }
+
+
+    public List<String[]> searchGroupMember (int groupID, String name) {
+        List<String[]> mem = new ArrayList<>();
+        String[] colName = {"Username", "Name", "Location", "Gender", "Birthdate"};
+        mem.add(colName);
+        try {
+            String sql = "SELECT Username, Name, City, Gender, Birthdate\n" +
+                    "FROM traveller T, group_member_travellers G\n" +
+                    "WHERE G.groupID = ? AND T.name = ?";
+            PreparedStatement prepState;
+            prepState = connection.prepareStatement(sql);
+            prepState.setString(1, name);
+            ResultSet rs = prepState.executeQuery();
+            while (rs.next()) {
+                String[] row = new String[colName.length];
+                row[0] = rs.getString("Username");
+                row[1] = rs.getString("City");
+                row[2] = rs.getString("Gender");
+                row[3] = rs.getString("Birthdate");
+                mem.add(row);
+            }
+
+        } catch (Exception e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+        return mem;
     }
 
 
