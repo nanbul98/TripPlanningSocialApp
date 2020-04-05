ffd9474e444fb9304e48a496d96493f473df1e28
Nandini Bulusu <nanbul785@gmail.com>
2020-04-04 17:40:31 -0700

    Linked to All Travellers page

--- src/ca/ubc/cs304/controller/TripApp.java
+++ src/ca/ubc/cs304/controller/TripApp.java
@@ -1,20 +1,21 @@
 package ca.ubc.cs304.controller;
 
 import ca.ubc.cs304.database.NewDatabaseConnectionHandler;
-import ca.ubc.cs304.delegates.*;
+import ca.ubc.cs304.delegates.AllTravellersDelegate;
 import ca.ubc.cs304.delegates.LoginWindowDelegate;
 import ca.ubc.cs304.delegates.MainWindowDelegate;
+import ca.ubc.cs304.ui.AllTravellers;
 import ca.ubc.cs304.ui.LoginWindow;
 import ca.ubc.cs304.ui.MainWindow;
-import ca.ubc.cs304.ui.TerminalTransactions;
 
 /**
  * This is the main controller class that will orchestrate everything.
  */
-public class TripApp implements LoginWindowDelegate, MainWindowDelegate {
+public class TripApp implements LoginWindowDelegate, MainWindowDelegate, AllTravellersDelegate {
     private NewDatabaseConnectionHandler dbHandler = null;
     private LoginWindow loginWindow = null;
     private MainWindow mainWindow = null;
+    private AllTravellers allTravellers = null;
 
 
     public TripApp() {
@@ -25,7 +26,7 @@ public class TripApp implements LoginWindowDelegate, MainWindowDelegate {
         loginWindow = new LoginWindow();
         loginWindow.showFrame(this);
         //login automatically
-        this.testLogin("ora_yeramko", "a80591878");
+//        this.testLogin("ora_yeramko", "a80591878");
     }
 
     /**
@@ -98,4 +99,11 @@ public class TripApp implements LoginWindowDelegate, MainWindowDelegate {
     }
 
 
+
+    public void goToTravellersPage() {
+        mainWindow.dispose();
+        allTravellers = new AllTravellers();
+        allTravellers.showFrame(this);
+
+    }
 }
